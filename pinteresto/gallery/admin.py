5a8c68d7d85ba0073931d4927753c16c70901f12
azamat <azaoroz147@gmail.com>
2022-01-18 20:29:01 +0600

    feature/DjangoPinterest/user&author/gitignore222

--- pinteresto/gallery/admin.py
+++ pinteresto/gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, News, Views, Likes, Tegs
+
+admin.site.register(Category)
+admin.site.register(News)
+admin.site.register(Views)
+admin.site.register(Likes)
+admin.site.register(Tegs)
--- pinteresto/gallery/models.py
+++ pinteresto/gallery/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-<<<<<<< HEAD
+
 # Create your models here.
 
 
@@ -12,8 +12,8 @@ class News(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    teg = models.ForeignKey('Tegs', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='category')
+    teg = models.ForeignKey('Tegs', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -29,58 +29,8 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
-=======
-
-class Blog(models.Model):
-    title = models.CharField(
-        max_length=160,
-        verbose_name='Заголовок',
-        db_index=True)
-    text = models.TextField(
-        blank=True,
-        verbose_name='Контент')
-    image = models.ImageField(
-        upload_to='image/%Y/%m/%d',
-        verbose_name='Изображение')
-    publish_date = models.DateTimeField(
-        auto_now=True,
-        verbose_name='Дата Публикации')
-    author = models.ForeignKey(
-        User,
-        on_delete=models.DO_NOTHING,
-        verbose_name='Автор')
-    is_published = models.BooleanField(
-        default=True,
-        verbose_name='Состаяние публикации')
-    category = models.ForeignKey(
-        "Category",
-        on_delete=models.DO_NOTHING,
-        verbose_name='Категория')
-
-    class Meta:
-        verbose_name = 'Блог'
-        verbose_name_plural = 'Блоги'
-        ordering = ['-publish_date']
-
-    def __str__(self):
-        return self.title
 
 
-class Category(models.Model):
-    title = models.CharField(
-        max_length=220,
-        verbose_name='Категории')
-    slug = models.SlugField(
-        max_length=220,
-        verbose_name='Url')
->>>>>>> 96ddef49809f5bbf9b3fa24fbe319dee62809744
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['-title']
-
-<<<<<<< HEAD
 
 class Views(models.Model):
     title = models.CharField(max_length=20)
@@ -123,10 +73,9 @@ class Tegs(models.Model):
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
         ordering = ['-teg']
-=======
-    def __str__(self):
-        return self.title
 
-    def get_absolute_url(self):
-        return reverse('category', kwargs={'slug': self.slug})
->>>>>>> 96ddef49809f5bbf9b3fa24fbe319dee62809744
+    def __str__(self):
+        return self.teg
+    #
+    # def get_absolute_url(self):
+    #     return reverse('category', kwargs={'slug': self.slug})
